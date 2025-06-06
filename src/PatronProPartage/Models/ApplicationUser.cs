51f7acd4da996f8d77ae94e7155d5884487dd94f
SouffleteauJordan <jordan.souffleteau@gmail.com>
2025-06-06 17:29:29 +0200

    Add basic role management using ASP.NET Core Identity

---
+++ src/PatronProPartage/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PatronProPartage.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    // Additional properties specific to the domain can be added here later
+}
--- src/PatronProPartage/Program.cs
+++ src/PatronProPartage/Program.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PatronProPartage.Data;
+using PatronProPartage.Models;
 using PatronProPartage.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Database context for Identity and other entities
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("PatronProPartage"));
+
+// ASP.NET Core Identity with role support
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -24,7 +37,21 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapRazorPages();
+
+await SeedRoles(app.Services);
 
 app.Run();
+
+static async Task SeedRoles(IServiceProvider services)
+{
+    using var scope = services.CreateScope();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    await IdentityDataInitializer.SeedAsync(userManager, roleManager);
+}
